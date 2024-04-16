ccf2c371a9612166f98b3c60092539b39abb9ade
ushakoviii <ushakov.ii@yandex.ru>
2024-04-16 21:49:46 +0300

    commit

--- src/components/GlobalStyles.tsx
+++ src/components/GlobalStyles.tsx
@@ -9,6 +9,21 @@ export const GlobalStyles = createGlobalStyle`
     margin: 0px;
     box-sizing: border-box;
   }
+  *::-webkit-scrollbar {
+    width: 17px;
+  }
+  *::-webkit-scrollbar-track {
+    background-color: ${myTheme.colors.secondColor};
+  }
+  *::-webkit-scrollbar-thumb {
+    background-color: ${myTheme.colors.mainColor};
+    border-radius: 5px;
+    border: 2px solid ${myTheme.colors.secondColor}
+  }
+  *::-webkit-scrollbar-button {
+    background-color: ${myTheme.colors.mainColor};
+    border: 2px solid ${myTheme.colors.secondColor}
+  }
   body {
     background-color: ${myTheme.colors.bcgColor};
     font-family: Poppins, sans-serif;
